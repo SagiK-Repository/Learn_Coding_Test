7df59349a002eea422f5754134a571fc90d3ca7b
박주형 <juhyung.park@mirero.co.kr>
2024-07-15 13:18:39 +0900

    feat : add base ball cpp

--- BeakjoonCpp/BeakjoonCpp/Master Algorithm (MA)/Number_Baseball.cpp
+++ BeakjoonCpp/BeakjoonCpp/Master Algorithm (MA)/Number_Baseball.cpp
@@ -11,25 +11,57 @@ struct BaseBall {
 
 struct TreeNode {
     string data;
-    vector<string> noneOne;
-    vector<string> noneTwo;
-    vector<string> noneThree;
+    vector<int> noneOne;
+    vector<int> noneTwo;
+    vector<int> noneThree;
     vector<TreeNode*> children;
 
     TreeNode(string dara
-           , vector<string> noneOne
-           , vector<string> noneTwo
-           , vector<string> noneThree) : data(data), noneOne(noneOne), noneTwo(noneTwo), noneThree(noneThree) {}
+           , vector<int> noneOne
+           , vector<int> noneTwo
+           , vector<int> noneThree) : data(data), noneOne(noneOne), noneTwo(noneTwo), noneThree(noneThree) {}
 };
 
-int Predicted_Cases(vector<BaseBall> input)
+static vector<int> ConvertStringToVector(const string& str) {
+    vector<int> result;
+    for (char c : str)
+        if (c != 'x')
+            result.push_back(c - '0');
+    return result;
+}
+
+static vector<int> GenerateNumbersExcept(const vector<int>& exclude) { // const : 변경 불가능, & : 복사해서 가져오지 않고, 원본을 가져온다.
+    vector<int> result;
+    for (int i = 0; i < 10; i++)
+        if (find(exclude.begin(), exclude.end(), i) == exclude.end())
+            result.push_back(i);
+    return result;
+}
+
+static string ChoiseString(string input, int choise1 = -1, int choise2 = -1) {
+    string output = "xxx";
+    if (choise1 != -1)
+        output[choise1] = input[choise1];
+    if (choise2 != -1)
+        output[choise2] = input[choise2];
+    return output;
+}
+
+static int Predicted_Cases(vector<BaseBall> input)
 {
-    vector<TreeNode> ansewers;
-    for (const auto& question : input)
-    {
-        if (question.strike == 0 && question.ball == 0)
-        {
-            TreeNode ansewer{"xxx", };
+    vector<TreeNode> answers;
+    for (const auto& question : input) {
+        if (question.strike == 0 && question.ball == 0) {
+            vector<int> exceptNumbers = ConvertStringToVector(question.number);
+            TreeNode answer{ "xxx", exceptNumbers, exceptNumbers, exceptNumbers };
+        }
+        else if (question.strike == 1 && question.ball == 0) {
+            for (int i = 0; i < 3; i++) {
+                string rightData = ChoiseString(question.number, i);
+                vector<int> rightException = ConvertStringToVector(rightData);
+                vector<int>  questionException = ConvertStringToVector(question.number);
+                TreeNode answer{ rightData, GenerateNumbersExcept(rightException), GenerateNumbersExcept(questionException) , GenerateNumbersExcept(questionException) };
+            }
         }
     }
     //    for (int strike = 0; strike < question.strike; strike++)
@@ -46,8 +78,7 @@ int main()
 
     vector<BaseBall> questions;
 
-    for (int index = 0; index < repeat; index++)
-    {
+    for (int index = 0; index < repeat; index++) {
         BaseBall question{};
         cin >> question.number >> question.strike >> question.ball;
         questions.push_back(question);
